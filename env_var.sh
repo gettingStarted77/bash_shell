10d2f4e5d91a265b79658383c2cd57c1424834c6
im <ispingzhou@gmail.com>
2023-01-11 20:15:11 +0800

    Improve commits and add PATH info in env_var.sh

--- env_var.sh
+++ env_var.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###
-# Add envirnoment variables to ~/.bash_profile.
+# Add setting to ~/.bash_profile for user login.
 # So you maybe need re-login to take it effect.
 ###
 
@@ -15,12 +15,16 @@ fi
 
 cat <<EOF | tee -a ~/.bash_profile
 
-# envirnoment variables from env_var.sh
+# envirnoment variables
 export EDITOR="nvim"
+
+# PATH
+# PATH="~/.cargo/bin:$PATH"
 
 # Autostart X at login. Place this in login shell initialization
 # file (e.g. ~/.bash_profile for Bash or ~/.zprofile for Zsh)
 #if [[ -z "${DISPLAY}" ]] && [[ "${XDG_VTNR}" -eq 1 ]]; then
 #  exec startx
 #fi
+
 EOF
